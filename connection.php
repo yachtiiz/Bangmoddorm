681222907d77d5b868300248223ffce04cc5911f
Yachtiiz <Yachtiiz@10.5.15.219>
2014-11-04 22:28:19 +0700

    Merge origin/master
    Conflicts:
    	dormdetail.php

--- connection.php
+++ connection.php
@@ -1,6 +1,6 @@
 <?php
 
 //Connection
-$con = mysqli_connect("localhost", "root", "", "BangmodDorm") or die("cant connected");
+$con = mysqli_connect("localhost", "backeyefin_bmd", "iamIt17!", "backeyefin_BMD") or die("cant connected");
 mysqli_query($con, "SET NAMES UTF8");
 ?>
